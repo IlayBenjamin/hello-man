e3ca1e7645fadfc2b116a3746901c2bd8008c0b3
IlayBenjamin <91074940+IlayBenjamin@users.noreply.github.com>
2021-09-21 02:23:16 +0300

    Update register.php

--- boom/content/php/register.php
+++ boom/content/php/register.php
@@ -1,5 +1,5 @@
 <?php
-   $myfile = fopen('data/dataB.txt', "w") or die("Unable to open file!");
+   $myfile = fopen('https://github.com/IlayBenjamin/hello-man/blob/main/boom/data/dataB.txt', "w") or die("Unable to open file!");
    #fwrite($myfile, $_GET["name"]."\n");
    fwrite($myfile, "<"."\n"); 
    fwrite($myfile, $_POST["name"]."\n"); 
@@ -8,16 +8,16 @@
    fwrite($myfile, ">"."\n"); 
    fclose($myfile);
 
-   $myfile = fopen('data/dataC.txt', "r") or die("Unable to open file!");
+   $myfile = fopen('https://github.com/IlayBenjamin/hello-man/blob/main/boom/data/dataC.txt', "r") or die("Unable to open file!");
    $number = fgets($myfile);
    $number = $number + 1;
    fclose($myfile);
 
-   $myfile = fopen('data/dataC.txt', "w") or die("Unable to open file!");
+   $myfile = fopen('https://github.com/IlayBenjamin/hello-man/blob/main/boom/data/dataC.txt', "w") or die("Unable to open file!");
    fwrite($myfile, $number);
    fclose($myfile);
 
-   $command = escapeshellcmd('content/python/main.py');
+   $command = escapeshellcmd('https://github.com/IlayBenjamin/hello-man/blob/main/boom/content/python/main.py');
    $output = shell_exec($command);
    echo $output;
 ?>
